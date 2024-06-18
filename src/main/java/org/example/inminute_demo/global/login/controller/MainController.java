34cdc760bb3ea1bcc167ebe5c2c9eabc53e20d59
woogieon8on <swp0927@naver.com>
2024-06-19 01:47:41 +0900

    Feat: oauth2 google, naver 로그인 구현

---
+++ src/main/java/org/example/inminute_demo/global/login/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.example.inminute_demo.global.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String mainAPI() {
+
+        return "main route";
+    }
+}
