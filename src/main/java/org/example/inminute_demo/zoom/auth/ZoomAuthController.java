16ddc3084ef6c74e511fe9dadd2c63cf3a60168f
woogieon8on <swp0927@naver.com>
2024-06-20 18:21:01 +0900

    Feat: zoom 로그인 리다이렉트 추가

--- src/main/java/org/example/inminute_demo/zoom/auth/ZoomAuthController.java
+++ src/main/java/org/example/inminute_demo/zoom/auth/ZoomAuthController.java
@@ -1,6 +1,8 @@
 package org.example.inminute_demo.zoom.auth;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.example.inminute_demo.global.apipayload.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,20 +22,25 @@ public class ZoomAuthController {
 
     // access code endpoint 입력하면 자동으로 호출 -> ZoomToken 엔티티에 엑세스 토큰, 리프레시 토큰 저장 완료
     @GetMapping("/zoomApi")
-    public ZoomToken getZoomToken(@RequestParam String code) throws IOException, NoSuchAlgorithmException {
+    public ApiResponse<?> getZoomToken(@RequestParam String code, @RequestParam Long noteId, HttpServletResponse response) throws IOException, NoSuchAlgorithmException {
         String redirectUri = "http://localhost:8080/zoomApi";
         String clientId = "G1cmu02jTSaxutApfmFYVA";
         String clientSecret = "sZFX1x0JC2fOG2rDUr09iM68Ts6tgA9j";
 
-        return zoomAuthService.getAccessToken(code, redirectUri, clientId, clientSecret);
+        zoomAuthService.getAccessToken(code, redirectUri, clientId, clientSecret);
+        response.sendRedirect("http://localhost:5137/note/" + noteId);
+
+        return ApiResponse.onSuccess("Access Token 발급됨");
     }
 
     // access token 만료 시 호출하여 재발급
     @GetMapping("/zoomApi/refresh")
-    public String refreshZoomToken() throws IOException {
+    public ApiResponse<?> refreshZoomToken() throws IOException {
         String clientId = "G1cmu02jTSaxutApfmFYVA";
         String clientSecret = "sZFX1x0JC2fOG2rDUr09iM68Ts6tgA9j";
 
-        return zoomAuthService.refreshToken(clientId, clientSecret);
+        zoomAuthService.refreshToken(clientId, clientSecret);
+
+        return ApiResponse.onSuccess("Access Token 재발급됨");
     }
 }
