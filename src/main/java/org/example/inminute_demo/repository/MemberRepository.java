71f4f56b3c341dfa09b1b8ebd36fffaf698f2f9c
woogieon8on <swp0927@naver.com>
2024-10-11 19:29:31 +0900

    #34 Feat: MemberService 구현

--- src/main/java/org/example/inminute_demo/repository/MemberRepository.java
+++ src/main/java/org/example/inminute_demo/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package org.example.inminute_demo.repository;
 import org.example.inminute_demo.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
