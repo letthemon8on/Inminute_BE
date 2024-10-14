0b188b37c45e30f6c31c6d974c6a5cea57875767
woogieon8on <swp0927@naver.com>
2024-10-14 23:19:11 +0900

    #25 Fix: 도메인 간 쿠키 공유 설정

--- src/main/java/org/example/inminute_demo/security/service/TokenService.java
+++ src/main/java/org/example/inminute_demo/security/service/TokenService.java
@@ -24,10 +24,10 @@ public class TokenService {
 
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60 * 60 * 60);
-        //cookie.setSecure(true); //https를 사용할 경우
+        cookie.setSecure(true); // https를 사용할 경우
         cookie.setPath("/"); // 쿠키가 적용될 경로
+        cookie.setDomain(".inminute.kr"); // 루트 도메인과 서브도메인에서 쿠키 공유
         cookie.setHttpOnly(true);
-
         return cookie;
     }
 
