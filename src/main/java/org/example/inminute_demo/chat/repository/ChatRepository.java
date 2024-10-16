7a7cecd11096f48bbbb6da147f7b0e2bf83a8396
woogieon8on <swp0927@naver.com>
2024-10-16 22:21:10 +0900

    #44 Feat: ChatRepository 구현

--- src/main/java/org/example/inminute_demo/chat/repository/ChatRepository.java
+++ src/main/java/org/example/inminute_demo/chat/repository/ChatRepository.java
@@ -1,9 +1,35 @@
 package org.example.inminute_demo.chat.repository;
 
 import org.example.inminute_demo.chat.domain.Chat;
+import org.example.inminute_demo.chat.dto.response.ChatResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
+    // DTO로 반환
+    @Query("""
+			SELECT 
+			new org.example.inminute_demo.chat.dto.response.ChatResponse
+			(c.id, u.id, u.nickname, c.type, c.created_at, c.content) 
+			FROM Chat c 
+			JOIN Member u ON u.id = c.memberId 
+			WHERE c.noteId = :noteId
+		""")
+    Page<ChatResponse> findByNoteId(@Param("noteId") Long noteId, Pageable pageable);
 
+    @Query("""
+			SELECT 
+			new org.example.inminute_demo.chat.dto.response.ChatResponse
+			(c.id, u.id, u.nickname, c.type, c.created_at, c.content) 
+			FROM Chat c 
+			JOIN Member u ON u.id = c.memberId 
+			WHERE c.noteId = :noteId
+		""")
+    List<ChatResponse> findAllByNoteId(@Param("noteId") Long noteId);
 }
