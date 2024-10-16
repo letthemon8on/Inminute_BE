a26952b9a643232a65e26a3509d670759740836d
woogieon8on <swp0927@naver.com>
2024-10-16 23:22:59 +0900

    #44 Feat: ChatService 구현

--- src/main/java/org/example/inminute_demo/chat/service/ChatService.java
+++ src/main/java/org/example/inminute_demo/chat/service/ChatService.java
@@ -15,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Map;
 
+import static org.example.inminute_demo.chat.converter.ChatConverter.*;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -24,11 +26,11 @@ public class ChatService {
 
     // 채팅 내역 저장
     @Transactional
-    public ChatResponse save(ChatRequest chatRequest, Long noteId, Map<String, Object> header) {
-        Chat chat = ChatConverter.toChat(chatRequest, noteId);
+    public ChatResponse save(ChatRequest chatRequest, Long noteId, String username) {
+        Chat chat = toChat(chatRequest, noteId);
         Chat savedChat = chatRepository.save(chat);
 
-        return toChatResponse(savedChat, header);
+        return toChatResponse(savedChat, username);
     }
 
     // 채팅 내역 조회(페이징)
@@ -40,16 +42,5 @@ public class ChatService {
     // 모든 채팅 내역 조회
     public List<ChatResponse> getAllByNoteId(Long noteId) {
         return chatRepository.findAllByNoteId(noteId);
-    }
-
-    private ChatResponse toChatResponse(Chat chat, Map<String, Object> header) {
-        String username = getValueFromHeader(header, "username");
-        String profileImgUrl = getValueFromHeader(header, "profileImgUrl");
-
-        return ChatConverter.toChatResponse(chat, username);
-    }
-
-    private String getValueFromHeader(Map<String, Object> header, String key) {
-        return (String)header.get(key);
     }
 }
