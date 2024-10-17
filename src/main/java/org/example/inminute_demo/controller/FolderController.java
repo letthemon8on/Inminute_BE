b7e0eeffa8da8ff69432a04080f1672356011e80
woogieon8on <swp0927@naver.com>
2024-10-17 16:46:08 +0900

    #28 Refactor: API url에서 /api 경로 제외

--- src/main/java/org/example/inminute_demo/controller/FolderController.java
+++ src/main/java/org/example/inminute_demo/controller/FolderController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Folder", description = "Folder 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/folders")
+@RequestMapping("/folders")
 public class FolderController {
 
     private final FolderService folderService;
