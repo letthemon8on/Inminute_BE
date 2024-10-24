bd66f548b0bca5bf36ecebb847dde50d3ecdf038
woogieon8on <140885810+woogieon8on@users.noreply.github.com>
2024-10-24 19:57:39 +0900

    Feature/46 (#49)
    * #46 Feat WebSocket 기반 오디오 스트림 전송 구현(미완성)
    
    * #46 Refactor: WebSocketStompConfig 이름 변경
    
    * #46 Feat: 변환된 텍스트 저장 기능 추가
    
    * #46 Feat: 오디오 스트림 예외처리 추가

--- src/main/java/org/example/inminute_demo/chat/converter/ChatConverter.java
+++ src/main/java/org/example/inminute_demo/chat/converter/ChatConverter.java
@@ -1,6 +1,7 @@
 package org.example.inminute_demo.chat.converter;
 
 import org.example.inminute_demo.chat.domain.Chat;
+import org.example.inminute_demo.chat.domain.MessageType;
 import org.example.inminute_demo.chat.dto.request.ChatRequest;
 import org.example.inminute_demo.chat.dto.response.ChatResponse;
 
@@ -22,6 +23,15 @@ public class ChatConverter {
                 .type(chat.getType())
                 .createdAt(chat.getCreated_at())
                 .content(chat.getContent())
+                .build();
+    }
+
+    public static Chat toChatFromTranscript(String transcript, String username, String uuid) {
+        return Chat.builder()
+                .username(username)
+                .type(MessageType.CHAT)
+                .content(transcript)
+                .uuid(uuid)
                 .build();
     }
 }
