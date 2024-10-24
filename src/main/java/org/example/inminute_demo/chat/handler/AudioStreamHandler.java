0d63e136ab7ba84d3c6a3e8ffc44a27e53a40f8b
woogieon8on <swp0927@naver.com>
2024-10-24 20:41:28 +0900

    #46 Feat: ChatRequest DTO 형식 적용

--- src/main/java/org/example/inminute_demo/chat/handler/AudioStreamHandler.java
+++ src/main/java/org/example/inminute_demo/chat/handler/AudioStreamHandler.java
@@ -3,16 +3,16 @@ package org.example.inminute_demo.chat.handler;
 import com.google.cloud.speech.v1.*;
 import com.google.protobuf.ByteString;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.example.inminute_demo.chat.converter.ChatConverter;
 import org.example.inminute_demo.chat.domain.Chat;
+import org.example.inminute_demo.chat.domain.MessageType;
+import org.example.inminute_demo.chat.dto.request.ChatRequest;
 import org.example.inminute_demo.chat.exception.WebSocketException;
 import org.example.inminute_demo.chat.repository.ChatRepository;
 import org.example.inminute_demo.domain.Member;
 import org.example.inminute_demo.repository.MemberRepository;
 import org.example.inminute_demo.security.dto.CustomOAuth2User;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.socket.BinaryMessage;
@@ -22,7 +22,6 @@ import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 import java.io.ByteArrayOutputStream;
 import java.net.URI;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AudioStreamHandler extends BinaryWebSocketHandler {
@@ -46,12 +45,20 @@ public class AudioStreamHandler extends BinaryWebSocketHandler {
             // Google STT로 변환
             String transcript = transcribeAudio(byteArrayOutputStream.toByteArray());
 
+            // WebSocket 세션에서 사용자 인증정보 가져오기
+            CustomOAuth2User customOAuth2User = (CustomOAuth2User) session.getPrincipal();
+            Member member = memberRepository.findByUsername(customOAuth2User.getUsername())
+                    .orElseThrow(() -> new WebSocketException("사용자 정보를 찾을 수 없습니다."));
+
             // 특정 채팅방 구독자들에게 변환된 텍스트를 메시지로 전송
             String uuid = parseUUIDFromPath(session);
-            messagingTemplate.convertAndSend("/topic/public/" + uuid, transcript);
+            ChatRequest chatRequest = new ChatRequest(
+                    MessageType.CHAT, member.getNickname(), transcript
+            );
+            messagingTemplate.convertAndSend("/topic/public/" + uuid, chatRequest);
 
             // 변환된 텍스트 Chat 엔티티로 저장
-            saveTranscript(session, transcript, uuid);
+            saveTranscript(member.getUsername(), transcript, uuid);
 
             byteArrayOutputStream.reset(); // 스트림 초기화
 
@@ -61,7 +68,6 @@ public class AudioStreamHandler extends BinaryWebSocketHandler {
     }
 
     private String parseUUIDFromPath(WebSocketSession session) {
-
         URI uri = session.getUri();
         // URI에서 UUID 부분 추출 (예: "/audio-stream/{uuid}")
         String path = uri.getPath();
@@ -70,20 +76,13 @@ public class AudioStreamHandler extends BinaryWebSocketHandler {
     }
 
     @Transactional
-    protected void saveTranscript(WebSocketSession session, String transcript, String uuid) {
-
-        CustomOAuth2User customOAuth2User = (CustomOAuth2User) session.getPrincipal();
-
-        Member member = memberRepository.findByUsername(customOAuth2User.getUsername())
-                .orElseThrow(() -> new WebSocketException("사용자 정보를 찾을 수 없습니다."));
-
-        Chat chat = ChatConverter.toChatFromTranscript(member.getUsername(), transcript, uuid);
+    protected void saveTranscript(String username, String transcript, String uuid) {
+        Chat chat = ChatConverter.toChatFromTranscript(username, transcript, uuid);
         chatRepository.save(chat);
     }
 
     // Google STT로 오디오 데이터를 변환하는 메소드
     private String transcribeAudio(byte[] audioData) throws Exception {
-
         // 클라이언트 인스턴스화
         try (SpeechClient speechClient = SpeechClient.create()) {
 
