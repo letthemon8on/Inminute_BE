5d20f84537a1982ac66e0d349e02df16ad8d9c77
woogieon8on <swp0927@naver.com>
2024-11-12 15:38:09 +0900

    Fix: CORS 에러 해결

--- src/main/java/org/example/inminute_demo/config/CorsMvcConfig.java
+++ src/main/java/org/example/inminute_demo/config/CorsMvcConfig.java
@@ -13,6 +13,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie")
                 .allowedOrigins("http://localhost:3000", "http://inminute.kr", "http://api.inminute.kr",
-                        "https://inminute.kr", "https://api.inminute.kr");
+                        "https://inminute.kr", "https://api.inminute.kr")
+                .allowCredentials(true);
     }
 }
