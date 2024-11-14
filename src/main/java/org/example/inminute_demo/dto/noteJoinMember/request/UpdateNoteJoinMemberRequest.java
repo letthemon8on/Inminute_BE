2637d033154e1fb2cdc73924593a66fab26f5d66
woogieon8on <swp0927@naver.com>
2024-11-15 02:47:29 +0900

    #59 Refactor: 회의 참여자별 요약 수정 기능 리팩토링

--- src/main/java/org/example/inminute_demo/dto/noteJoinMember/request/UpdateNoteJoinMemberRequest.java
+++ src/main/java/org/example/inminute_demo/dto/noteJoinMember/request/UpdateNoteJoinMemberRequest.java
@@ -1,16 +1,7 @@
 package org.example.inminute_demo.dto.noteJoinMember.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class UpdateNoteJoinMemberRequest {
-
-    private String summary;
-    private String todo;
+public record UpdateNoteJoinMemberRequest(
+        Long noteJoinMemberId,
+        String summary
+) {
 }
