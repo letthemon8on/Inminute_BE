4509d409af4ec410be799808eddf502733e09305
woogieon8on <swp0927@naver.com>
2024-11-26 22:57:10 +0900

    Fix: Transactional 적용

--- src/main/java/org/example/inminute_demo/chat/service/ChatService.java
+++ src/main/java/org/example/inminute_demo/chat/service/ChatService.java
@@ -160,12 +160,14 @@ public class ChatService {
         return toChatResponse(chat, header);
     }
 
+    @Transactional
     public ChatStatusResponse startChatting(String uuid, Map<String, Object> header) {
 
         noteService.updateIsStart(uuid, true);
         return toChatStatusResponse(true, header);
     }
 
+    @Transactional
     public ChatStatusResponse stopChattingStatus(String uuid, Map<String, Object> header) {
 
         noteService.updateIsStart(uuid, false);
