ba50530facd53da7cffc4b0633366a8c46d6fbe3
woogieon8on <swp0927@naver.com>
2024-11-28 02:20:06 +0900

    Refactor: 불필요한 import 제거

--- src/main/java/org/example/inminute_demo/service/ScheduleService.java
+++ src/main/java/org/example/inminute_demo/service/ScheduleService.java
@@ -16,7 +16,6 @@ import org.example.inminute_demo.security.dto.CustomOAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
